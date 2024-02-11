69c9cbb493151d7d9cb2f9b6ef1b9612545a0cd1
ppp <philipp.panhey@gmail.com>
2024-02-11 21:21:50 +0100

    added humidity and options as commanline

--- Program.cs
+++ Program.cs
@@ -1,26 +1,66 @@
 ﻿using HtmlAgilityPack;
+using CommandLine;
 
-string url = "https://www.ach-du-schan.de/";
-
-string html = getHtml(url);
-double temperature = getTemperature(html);
-Console.WriteLine(temperature);
-
-double getTemperature(string html)
+public class Options
 {
-    var htmlDoc = new HtmlDocument();
-    htmlDoc.LoadHtml(html);
-    var fontNode = htmlDoc.DocumentNode.SelectSingleNode("//font[@face='Arial'][@color='#FF0000'][@size='7']");
-    return Double.Parse(fontNode.InnerHtml.Replace("�C", ""));
+    [Option('t', "temperature", Required = false, HelpText = "get temperature.")]
+    public bool Temperature { get; set; } = false;
+
+    [Option('h', "humidity", Required = false, HelpText = "get humidity.")]
+    public bool Humidity { get; set; } = false;
 }
-
-string getHtml(string url)
+class Program
 {
-    using (HttpClient client = new HttpClient())
+    private const string url = "https://www.ach-du-schan.de/";
+
+    static void Main(string[] args)
     {
-        HttpResponseMessage response = client.GetAsync(url).Result;
-        response.EnsureSuccessStatusCode();
-        string responseBody = response.Content.ReadAsStringAsync().Result;
-        return responseBody;
+        var parser = new CommandLine.Parser(with => with.HelpWriter = Console.Out);
+        var result = parser.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                string html = GetHtml(url);
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(html);
+                if (o.Temperature)
+                {
+                    string temperature = GetTemperature(htmlDoc).ToString();
+                    Console.WriteLine($"Temperature: {temperature}");
+                }
+
+                if (o.Humidity)
+                {
+                    string humidity = GetHumidity(htmlDoc).ToString();
+                    Console.WriteLine($"Humidity: {humidity}");
+                }
+            });
+
+    }
+    static string GetHtml(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+            return responseBody;
+        }
+    }
+
+    static double GetTemperature(HtmlDocument htmlDoc)
+    {
+        var singlenodecontent = SingleNodeContent(htmlDoc, "//font[@face='Arial'][@color='#FF0000'][@size='7']");
+        return Double.Parse(singlenodecontent.Replace("�C", ""));
+    }
+    static double GetHumidity(HtmlDocument htmlDoc)
+    {
+        string singlenodecontent = SingleNodeContent(htmlDoc, "//font[@face='Arial'][@color='#0080FF'][@size='7']");
+        return Double.Parse(singlenodecontent.Replace("%", ""));
+    }
+
+    static string SingleNodeContent(HtmlDocument htmlDoc, string singleNode)
+    {
+        var node = htmlDoc.DocumentNode.SelectSingleNode(singleNode);
+        return node.InnerHtml;
     }
 }
